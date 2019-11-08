3f85544e6b979ce10a02917ee979456b203070ab
Spencer DeBuf <spencer602@gmail.com>
2019-11-07 17:23:10 -0700

    Initial Commit

---
+++ shark-hunt/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  shark-hunt
+//
+//  Created by Spencer DeBuf on 11/7/19.
+//  Copyright © 2019 Spencer DeBuf. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
