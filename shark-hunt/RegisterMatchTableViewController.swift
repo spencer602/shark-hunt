b5242b0564dcefa986e3d518800d7d99d45e5a03
Spencer DeBuf <spencer602@gmail.com>
2019-11-12 13:50:54 -0700

    location picker working

--- shark-hunt/RegisterMatchTableViewController.swift
+++ shark-hunt/RegisterMatchTableViewController.swift
@@ -11,6 +11,7 @@ import UIKit
 class RegisterMatchTableViewController: UITableViewController, UIPickerViewDelegate, UIPickerViewDataSource {
     
     var players = [PlayerModel]()
+    var locations = [String]()
     
     func reloadPlayerData() {
         let urlPath: String = Settings.urlStringPrefix + "currentstandingsjson.php"
@@ -56,6 +57,47 @@ class RegisterMatchTableViewController: UITableViewController, UIPickerViewDeleg
                     self.players = allPlayers
                     self.player1Picker.reloadAllComponents()
                     self.player2Picker.reloadAllComponents()
+                })
+            }
+        }
+        task.resume()
+    }
+    
+    func reloadLocationsData() {
+        let urlPath: String = Settings.urlStringPrefix + "alllocationjson.php"
+        let url: URL = URL(string: urlPath)!
+        let defaultSession = Foundation.URLSession(configuration: URLSessionConfiguration.default)
+        
+        let task = defaultSession.dataTask(with: url) { (data, response, error) in
+            if error != nil {
+                print("Failed to download data")
+            }else {
+                print("Data downloaded")
+               
+                var jsonResult = NSArray()
+                                                
+                do {
+                    jsonResult = try JSONSerialization.jsonObject(with: data!, options:JSONSerialization.ReadingOptions.allowFragments) as! NSArray
+                } catch let error as NSError {
+                    print(error)
+                }
+                
+                var jsonElement = NSDictionary()
+                var allLocations = [String]()
+                
+                for i in 0 ..< jsonResult.count
+                {
+                    jsonElement = jsonResult[i] as! NSDictionary
+                    
+                    //the following insures none of the JsonElement values are nil through optional binding
+                    if let name = jsonElement["location_name"] as? String
+                    {
+                        allLocations.append(name)
+                    }
+                }
+                DispatchQueue.main.async(execute: { () -> Void in
+                    self.locations = allLocations
+                    self.locationPicker.reloadAllComponents()
                 })
             }
         }
@@ -167,7 +209,8 @@ class RegisterMatchTableViewController: UITableViewController, UIPickerViewDeleg
             "playerOnePointsInput": Int(p1PointsStepper.value),
             "playerTwoPointsInput": Int(p2PointsStepper.value),
             "playerOneGamesWonInput": Int(p1GamesWonStepper.value),
-            "playerTwoGamesWonInput": Int(p2GamesWonStepper.value)
+            "playerTwoGamesWonInput": Int(p2GamesWonStepper.value),
+            "locationPlayed": locations[locationPicker.selectedRow(inComponent: 0)]
         ]
         
         let jsonData = try! JSONSerialization.data(withJSONObject: json, options: [])
@@ -248,14 +291,17 @@ class RegisterMatchTableViewController: UITableViewController, UIPickerViewDeleg
     }
     
     func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
-        return players.count;
+        if pickerView === player1Picker || pickerView === player2Picker { return players.count }
+        else if pickerView === locationPicker { return locations.count }
+        else { return 0 }
     }
     
     func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
-        return players[row].name;
+        if pickerView === player1Picker || pickerView === player2Picker { return players[row].name }
+        else if pickerView === locationPicker { return locations[row] }
+        else { return nil }
     }
     
-
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -265,10 +311,20 @@ class RegisterMatchTableViewController: UITableViewController, UIPickerViewDeleg
         player1Picker.dataSource = self
         player2Picker.delegate = self
         player2Picker.dataSource = self
+        locationPicker.delegate = self
+        locationPicker.dataSource = self
         
         reloadPlayerData()
+        reloadLocationsData()
 
         // Do any additional setup after loading the view.
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        reloadPlayerData()
+        reloadLocationsData()
     }
     
     func parseJSON(_ data: Data) {
