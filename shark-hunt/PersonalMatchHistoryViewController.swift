12abb2f5f8726ccd9959775f6596a49486c16c44
Spencer DeBuf <spencer602@gmail.com>
2019-11-17 11:20:37 -0700

    created class for personal match history

---
+++ shark-hunt/PersonalMatchHistoryViewController.swift
@@ -0,0 +1,32 @@
+//
+//  PersonalMatchHistoryViewController.swift
+//  shark-hunt
+//
+//  Created by Spencer DeBuf on 11/17/19.
+//  Copyright © 2019 Spencer DeBuf. All rights reserved.
+//
+
+import UIKit
+
+class PersonalMatchHistoryViewController: UIViewController {
+    
+    var player: PlayerModel!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
